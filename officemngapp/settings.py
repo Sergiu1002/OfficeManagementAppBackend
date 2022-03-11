e9dbdeff51f5bb69e4b92e82fbbdfa90a7bcdbda
Sergiu1002 <sergiutudosi@yahoo.com>
2022-03-11 18:29:56 +0200

    schimbat bd in postgresql

--- officemngapp/settings.py
+++ officemngapp/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'officemngapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'officemanagementapp',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
+        
+        }
 }
 
 
