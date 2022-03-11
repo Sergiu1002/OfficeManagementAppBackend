e0a44d95c6be2dd9781fe9c9faebe580f57351a6
Sergiu1002 <sergiutudosi@yahoo.com>
2022-03-11 23:49:43 +0200

    modificat tabel angajati

--- main/models.py
+++ main/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-class ToDoList(models.Model):
-    name = models.CharField(max_length = 200)
-    
-    def __str__(self):
-        return self.name
-
-class Item(models.Model):
-    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
-
 class angajati(models.Model):
     nume = models.CharField(max_length=15)
     prenume = models.CharField(max_length=15)
@@ -35,11 +20,10 @@ class angajati(models.Model):
         default=MALE,)
     
     email = models.EmailField()
-    nasterii = models.DateTimeField(auto_now=False, auto_now_add=False, default = timezone.now)
+    data_nasterii = models.DateTimeField(auto_now=False, auto_now_add=False, default = timezone.now)
     nationalitate = models.CharField(max_length=45)
     adresa = models.CharField(max_length=100)
 
-    #numele_complet = nume + prenume
     def __int__(self):
         return self.pk
 
--- main/views.py
+++ main/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import ToDoList, Item, angajati
-
-# Create your views here.
-def index(response, id):
-    ls = ToDoList.objects.get(id=id)
-    return render(response, "main/list.html", {"ls": ls})
+from .models import angajati
 
 def home(response):
     return render(response, "main/home.html", {"name": "ursu"})
-
-def create(response):
-    return render(response, "main/create.html", {"name": "kalu"})
